514b3684ed400c7fc81b75d4974fc47cf631a663
bas-ene <enea.basilico@gmail.com>
2023-12-12 19:14:04 +0100

    lol, errori scemi

--- chckInserisci.php
+++ chckInserisci.php
@@ -15,18 +15,15 @@ if ($_SESSION["idUtente"]) //controllo se è loggato
 //controlla che tutto e' settato, se no ritorna errore
 if (
     isset($_POST["Titolo"]) && isset($_POST["AnnoProduzione"]) && isset($_POST["Nazionalita"]) && isset($_POST["Durata"])
-    && isset($_POST["Regista"]) &&  isset($_FILES["image_path"]) && isset($_POST["Genere"])
+    && isset($_POST["Regista"]) && isset($_FILES["image_path"]) && isset($_POST["Genere"])
 ) {
     $fileName = basename($_FILES["image_path"]["name"]);
     $uploadPath = "images/" . $fileName;
     move_uploaded_file($_FILES["image_path"]["tmp_name"], $uploadPath);
     $timeValue = $conn->query("SELECT SEC_TO_TIME(" . $_POST['Durata'] . " * 60) AS formatted_time")->fetch_assoc()['formatted_time'];
-    echo $timeValue;
-    $insert_query = "INSERT INTO film (Titolo, AnnoProduzione, Nazionalita, Regista, Genere, Durata, image_path) VALUES (
-    " . $_POST['Titolo'] . "," . $_POST['AnnoProduzione'] . "," . $_POST['Nazionalita'] . "," . $_POST['Regista'] . "," . $_POST['Genere'] . "
-    " . $timeValue . "," . $fileName . " )";
+    $insert_query = "INSERT INTO film (Titolo, AnnoProduzione, Nazionalita, Regista, Genere, Durata, image_path) VALUES ('
+    " . $_POST['Titolo'] . "','" . $_POST['AnnoProduzione'] . "','" . $_POST['Nazionalita'] . "','" . $_POST['Regista'] . "','" . $_POST['Genere'] . "', \"
+    " . $timeValue . "\",\"" . $fileName . "\" )";
+    echo $insert_query;
     $result = $conn->query($insert_query);
-    echo $result->num_rows;
-    print_r($result);
 }
-echo "errore";
